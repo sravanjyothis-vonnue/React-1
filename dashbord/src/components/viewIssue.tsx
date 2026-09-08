0b851091aa79d64615f7e5d7ab7f6d9d55eae6c3
sravan <sravan.jyothis@vonnue.com>
2026-09-08 14:44:31 +0530

    feat : added nested route and validation to login form

--- dashbord/src/components/viewIssue.tsx
+++ dashbord/src/components/viewIssue.tsx
@@ -2,39 +2,47 @@ import { Header } from "./header";
 import issues from "../data/taskData.json";
 import { useParams } from "react-router-dom";
 
+function handleDeleteClick() {}
+
+function handleEditClick() {}
+
 export function ViewIssue() {
   const { id } = useParams();
   if (id == undefined) {
     throw new Error("Id parameter is undefined");
   }
-  const data = issues.filter((issue) => String(issue.issueId) == id);
-  if (data.length == 0) {
+  const data = issues.find((issue) => String(issue.issueId) == id);
+  if (!data) {
     throw new Error("No data found for given parameter");
   }
   return (
     <>
       <Header />
       <div className="headerContainer">
-        <div className="issueTitle">{data[0].title}</div>
+        <div className="issueTitle">{data.title}</div>
       </div>
       <div className="contentContainer">
         <div className="details">
           <div className="issueDue">
             <span className="brownText">DUE : </span>
-            {data[0].due}
+            {data.due}
           </div>
           <div className="issueAssignee">
             <span className="brownText">ASSIGNEE : </span>
-            {data[0].assignee}
+            {data.assignee}
           </div>
           <div className="issueStatus">
             <span className="brownText">STATUS : </span>
-            {data[0].status}
+            {data.status}
           </div>
         </div>
         <div className="buttons">
-          <button className="editIssue">Edit Issue</button>
-          <button className="deleteButton">Delete Issue</button>
+          <button className="editIssue" onClick={() => handleEditClick()}>
+            Edit Issue
+          </button>
+          <button className="deleteButton" onClick={() => handleDeleteClick()}>
+            Delete Issue
+          </button>
         </div>
       </div>
     </>
